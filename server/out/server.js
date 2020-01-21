a6d0488d1b339b04ea7ce2b3722148736234e4ac
Varuna Jayasiri <vpjayasiri@gmail.com>
2020-01-21 15:26:06 +0530

    🧹 remove js code

--- server/out/server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const node_1 = require("./io/node");
-const static_1 = require("./io/static");
-let SERVER = new node_1.NodeHttpServerPort(8088, null, true);
-exports.SERVER = SERVER;
-let STATIC_SERVER = new static_1.StaticServer('/Users/varuna/ml/annotate/ui/out', new Set(['/api']));
-exports.STATIC_SERVER = STATIC_SERVER;
-SERVER.handleRequest = STATIC_SERVER.handleRequest;
